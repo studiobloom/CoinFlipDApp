2fe20bc12d916c5d741be5a13cea5f5b75dbae7c
studiobloom <john.large.designs@gmail.com>
2023-05-25 22:06:55 -0700

    App.js Update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const contractAddress = "0x0cde2c81aeac28b672af6c97a9c631073fa8181e";
-const contractABI = [{"inputs":[{"internalType":"address","name":"_receiver","type":"address"}],"name":"placeBet","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"bets","outputs":[{"internalType":"address payable","name":"sender","type":"address"},{"internalType":"address payable","name":"receiver","type":"address"},{"internalType":"uint256","name":"betAmount","type":"uint256"},{"internalType":"uint256","name":"createdAt","type":"uint256"},{"internalType":"uint256","name":"acceptDeadline","type":"uint256"},{"internalType":"bool","name":"betAccepted","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"acceptBet","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"revokeBet","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getBetDetails","outputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint256","name":"betAmount","type":"uint256"},{"internalType":"uint256","name":"createdAt","type":"uint256"},{"internalType":"uint256","name":"acceptDeadline","type":"uint256"},{"internalType":"bool","name":"betAccepted","type":"bool"}],"stateMutability":"view","type":"function"}];
+const contractABI = [{"inputs":[{"internalType":"address","name":"_receiver","type":"address"}],"name":"placeBet","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"acceptBet","outputs":[],"stateMutability":"payable","type":"function"}];
+
 let contractInstance;
 
 window.addEventListener('load', async () => {
@@ -38,26 +39,3 @@ async function acceptBet(betIndex) {
   await contractInstance.methods.acceptBet(betIndex).send({from: account, value: betAmount});
   console.log('Bet accepted successfully!');
 }
-
-async function getBets() {
-  const betCount = await contract.methods.getBetCount().call();
-
-  // Clear the table
-  $("#bet-table tbody").empty();
-
-  // Add new bets to the table
-  for (let i = 0; i < betCount; i++) {
-    const bet = await contract.methods.getBets(i).call();
-    const createdAt = new Date(bet[2] * 1000).toLocaleString();
-
-    if (bet[0] != 0 && !bet[3]) { // Check if sender address is set and bet is not accepted yet
-      const row = $("<tr>").appendTo("#bet-table tbody");
-      $("<td>").text(i).appendTo(row);
-      $("<td>").text(bet[0]).appendTo(row);
-      $("<td>").text(web3.utils.fromWei(bet[1], "ether")).appendTo(row);
-      $("<td>").text(createdAt).appendTo(row);
-      $("<td>").html(`<button class="btn btn-primary btn-sm" onclick="acceptBet(${i})">Accept</button>`).appendTo(row);
-    }
-  }
-}
-
--- coinflip.sol
+++ coinflip.sol
@@ -1,100 +1,58 @@
-# Palkeoramix decompiler. 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-def storage:
-  senderAddress is addr at storage 0
-  receiverAddress is addr at storage 1
-  betAmount is uint256 at storage 2
-  createdAt is uint256 at storage 3
-  unknown1cb8c540 is uint256 at storage 4
-  unknownee9078a3 is uint8 at storage 5
+contract CoinFlipBet {
+    struct Bet {
+        address payable sender;
+        address payable receiver;
+        uint256 betAmount;
+        uint256 createdAt;
+        bool betAccepted;
+    }
 
-def unknown1cb8c540(): # not payable
-  return unknown1cb8c540
+    mapping(address => Bet[]) public bets;
 
-def sender(): # not payable
-  return senderAddress
+    event BetPlaced(address indexed _sender, address indexed _receiver, uint256 _betAmount);
+    event BetAccepted(address indexed _receiver, uint256 _betAmount);
+    event CoinFlipResult(address indexed _winner, uint256 _amount);
+    event WinnerPaid(address indexed _winner, uint256 _amount);
 
-def createdAt(): # not payable
-  return createdAt
+    function placeBet(address payable _receiver) external payable {
+        require(msg.value > 0, "Bet amount should be greater than 0");
 
-def betAmount(): # not payable
-  return betAmount
+        Bet memory newBet = Bet(payable(msg.sender), _receiver, msg.value, block.timestamp, false);
+        bets[_receiver].push(newBet);
 
-def unknownee9078a3(): # not payable
-  return bool(unknownee9078a3)
+        emit BetPlaced(newBet.sender, newBet.receiver, newBet.betAmount);
+    }
 
-def receiver(): # not payable
-  return receiverAddress
+    function acceptBet(uint256 _index) external payable {
+        require(_index < bets[msg.sender].length, "Invalid bet index");
+        Bet storage bet = bets[msg.sender][_index];
+        require(!bet.betAccepted, "Bet already accepted");
+        require(block.timestamp <= bet.createdAt + 3 days, "Bet acceptance deadline passed");
+        require(msg.value == bet.betAmount, "Incorrect bet amount");
 
-#
-#  Regular functions
-#
+        bet.betAccepted = true;
 
-def _fallback() payable: # default function
-  revert
+        emit BetAccepted(bet.receiver, bet.betAmount);
 
-def unknown2f790c79(): # not payable
-  if senderAddress != caller:
-      revert with 0, 'Only sender can revoke the bet'
-  if unknownee9078a3:
-      revert with 0, 'Bet already accepted'
-  if block.timestamp <= unknown1cb8c540:
-      revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Bet acceptance deadline not passed yet'
-  call senderAddress with:
-     value betAmount wei
-       gas 2300 * is_zero(value) wei
-  if not ext_call.success:
-      revert with ext_call.return_data[0 len return_data.size]
-  log 0x92c399bf: senderAddress
+        _flipCoinAndPayout(bet);
+    }
 
-def placeBet(address _miner) payable: 
-  require calldata.size - 4 >=ΓÇ▓ 32
-  require _miner == _miner
-  if senderAddress:
-      revert with 0, 'Bet already placed'
-  if call.value <= 0:
-      revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Bet amount should be greater than 0'
-  senderAddress = caller
-  receiverAddress = _miner
-  betAmount = call.value
-  createdAt = block.timestamp
-  if createdAt > createdAt + (72 * 24 * 3600):
-      revert with 'NH{q', 17
-  unknown1cb8c540 = createdAt + (72 * 24 * 3600)
-  log 0x3fe3739e: betAmount, senderAddress, receiverAddress
+    function _flipCoinAndPayout(Bet memory _bet) private {
+        uint256 winner = uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp))) % 2;
+        address payable winnerAddress;
 
-def unknown30bfcb5f() payable: 
-  if receiverAddress != caller:
-      revert with 0, 'Only receiver can accept the bet'
-  if unknownee9078a3:
-      revert with 0, 'Bet already accepted'
-  if block.timestamp > unknown1cb8c540:
-      revert with 0, 'Bet acceptance deadline passed'
-  if betAmount != call.value:
-      revert with 0, 'Incorrect bet amount'
-  unknownee9078a3 = 1
-  log 0xdd6dae32: receiverAddress
-  if betAmount and 2 * betAmount / betAmount != 2:
-      revert with 'NH{q', 17
-  if sha3(block.difficulty, block.timestamp) % 2:
-      log 0x90562fde: (2 * betAmount), receiverAddress
-      call receiverAddress with:
-         value eth.balance(this.address) wei
-           gas 2300 * is_zero(value) wei
-      if not ext_call.success:
-          revert with ext_call.return_data[0 len return_data.size]
-      if betAmount and 2 * betAmount / betAmount != 2:
-          revert with 'NH{q', 17
-      log 0x8cbbe5cd: (2 * betAmount), receiverAddress
-  else:
-      log 0x90562fde: (2 * betAmount), senderAddress
-      call senderAddress with:
-         value eth.balance(this.address) wei
-           gas 2300 * is_zero(value) wei
-      if not ext_call.success:
-          revert with ext_call.return_data[0 len return_data.size]
-      if betAmount and 2 * betAmount / betAmount != 2:
-          revert with 'NH{q', 17
-      log 0x8cbbe5cd: (2 * betAmount), senderAddress
+        if (winner == 0) {
+            winnerAddress = _bet.sender;
+        } else {
+            winnerAddress = _bet.receiver;
+        }
 
+        emit CoinFlipResult(winnerAddress, _bet.betAmount * 2);
 
+        winnerAddress.transfer(address(this).balance);
+        emit WinnerPaid(winnerAddress, _bet.betAmount * 2);
+    }
+}
