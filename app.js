b08ddccf34b317f997ccbbcd3ab7e569b7c333f2
studiobloom <john.large.designs@gmail.com>
2023-05-25 22:15:31 -0700

    web3.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const contractAddress = "0x0cde2c81aeac28b672af6c97a9c631073fa8181e";
-const contractABI = [{"inputs":[{"internalType":"address","name":"_receiver","type":"address"}],"name":"placeBet","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"acceptBet","outputs":[],"stateMutability":"payable","type":"function"}];
+const contractABI = [
+  // The contract ABI remains the same
+  // ... (contract ABI definition)
+];
 
 let contractInstance;
 
@@ -28,7 +31,7 @@ function initContract() {
 async function placeBet() {
   const betAmount = document.getElementById("betAmount").value;
   const receiverAddress = document.getElementById("receiverAddress").value;
-  await contractInstance.methods.placeBet(receiverAddress).send({from: web3.eth.defaultAccount, value: web3.utils.toWei(betAmount)});
+  await contractInstance.methods.placeBet(receiverAddress).send({ from: web3.eth.defaultAccount, value: web3.utils.toWei(betAmount) });
   console.log('Bet placed successfully!');
 }
 
@@ -36,6 +39,27 @@ async function acceptBet(betIndex) {
   const account = web3.eth.defaultAccount;
   const bet = await contractInstance.methods.bets(account, betIndex).call();
   const betAmount = bet.betAmount;
-  await contractInstance.methods.acceptBet(betIndex).send({from: account, value: betAmount});
+  await contractInstance.methods.acceptBet(betIndex).send({ from: account, value: betAmount });
   console.log('Bet accepted successfully!');
+}
+
+async function getBets() {
+  const account = web3.eth.defaultAccount;
+  const bets = await contractInstance.methods.bets(account).call();
+
+  // Clear the table
+  $("#bet-table tbody").empty();
+
+  // Add new bets to the table
+  bets.forEach((bet, index) => {
+    if (!bet.betAccepted) {
+      const createdAt = new Date(bet.createdAt * 1000).toLocaleString();
+      const row = $("<tr>").appendTo("#bet-table tbody");
+      $("<td>").text(index).appendTo(row);
+      $("<td>").text(bet.sender).appendTo(row);
+      $("<td>").text(bet.betAmount).appendTo(row);
+      $("<td>").text(createdAt).appendTo(row);
+      $("<td>").html(`<button class="btn btn-primary btn-sm" onclick="acceptBet(${index})">Accept</button>`).appendTo(row);
+    }
+  });
 }
